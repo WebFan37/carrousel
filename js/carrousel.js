aeb5766495bf5c53839ffb286582f8887951bb1e
WebFan37 <jeanneel2000@gmail.com>
2024-04-18 12:06:46 -0400

    Carrousel galery image generator with code organization, Carrousel placement small change, no animation.

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,14 +1,54 @@
 (function(){
     console.log("Vive Javascrip")
 
+    
+    /*************************************************** */
+    //boite carrousel
     let carrousel = document.querySelector('.carrousel')
     console.log(carrousel.tagName)
 
+    //bouton ouvrir
     let bouton = document.querySelector('.bouton__ouvrir')
     console.log(bouton.tagName)
 
+    //bouton fermer
     let boutonfermer = document.querySelector('.carrousel__x')
     console.log(boutonfermer.tagName)
+
+    //bouton galerie
+    let galerie = document.querySelector('.galerie')
+    console.log(galerie.tagName)
+
+    
+    
+    
+    //creation dynamique d'une image du carrousel//
+    //========================================//
+
+     //Figure Carrousel
+    let carrousel_figure = document.querySelector('.carrousel__figure')
+    carrousel_figure.appendChild(carrousel_img)
+    
+    //Image Carrousel (creation dynamique)
+    let carrousel_img = document.createElement('img')
+    carrousel_img.classList.add('carrousel__img')
+
+    //Image Galerie
+    let galerie_img = galerie.querySelectorAll("img")
+    console.log(galerie_img.src)
+
+    //Boucle affichage image galerie
+    for (const element of galerie_img) {
+        carrousel_img.src = element.src
+    }
+
+   
+    
+    //Afficher la premiere image de la galerie
+    // carrousel_img.src = galerie_img.src
+
+    /************************************************** */
+    /************************************************** */
 
     //Ouvrir la boite
     bouton.addEventListener('click', function(){
