fe77ea4ef96de5476b8a6580a53a1eef332987c4
WebFan37 <jeanneel2000@gmail.com>
2024-04-18 10:59:32 -0400

    Carrousel open and close javasc. Adding more style

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,3 +1,22 @@
 (function(){
     console.log("Vive Javascrip")
-})()
+
+    let carrousel = document.querySelector('.carrousel')
+    console.log(carrousel.tagName)
+
+    let bouton = document.querySelector('.bouton__ouvrir')
+    console.log(bouton.tagName)
+
+    let boutonfermer = document.querySelector('.carrousel__x')
+    console.log(boutonfermer.tagName)
+
+    //Ouvrir la boite
+    bouton.addEventListener('click', function(){
+        carrousel.classList.add('carrousel--ouvrir')
+    })
+
+    //Fermer la boite
+    boutonfermer.addEventListener('click', function(){
+        carrousel.classList.remove('carrousel--ouvrir')
+    })
+})
